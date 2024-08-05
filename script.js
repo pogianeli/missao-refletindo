cd219ee6a37f97d828009e3208208cd532ff949a
pogianeli <130576465+pogianeli@users.noreply.github.com>
2024-08-05 09:43:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saio da escola me depara com uma nova jornada, um futuro aonde coisas melhores virão, mas para isso terei que estudar muito e trabalhar duro. Qual é o meu primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Isso é demais, mas eu devo mesmo entrar nessa?",
+                afirmacao: "No início fico com medo, pois tudo depende apenas da minha capacidade. "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Isso é incrivel",
+                afirmacao: "Procurei saber como seria daqui pra frente, é como eu deveria agir."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Com a descoberta deste futuro,s",
         alternativas: [
             {
                 texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
